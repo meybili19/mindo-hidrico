cf9f12e9f91bd64410c859061af5c48a1a80a3f6
Meybili <109104859+meybili19@users.noreply.github.com>
2025-06-29 18:56:45 -0500

    feat:

--- src/app/prediccion/page.tsx
+++ src/app/prediccion/page.tsx
@@ -2,18 +2,18 @@ import PrediccionClimatica from "@/components/PrediccionClimatica";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 
-import styles from '../../styles/Prediccion.module.css';
+import styles from '../../styles/Prediccion.module.css';  // CSS del layout
 
 export default function Page() {
-  return (
-    <>
-      <Header />
-      <main className={styles.main}>
-        <div className={styles.container}>
-          <PrediccionClimatica />
+    return (
+        <div className={styles.pageWrapper}>
+            <Header />
+            <main className={styles.main}>
+                <div className={styles.container}>
+                    <PrediccionClimatica />
+                </div>
+            </main>
+            <Footer />
         </div>
-      </main>
-      <Footer />
-    </>
-  );
-}
+    );
+}   
